479c497d245d0d6e308d01a74f5a237f99d03b6b
Mykolas <101357393+MStasiulionis@users.noreply.github.com>
2022-09-06 21:23:28 +0300

    add

--- Flaskas/paskaita2/app.py
+++ Flaskas/paskaita2/app.py
@@ -1,22 +1,39 @@
-from flask import Flask, render_template
-
-from src.article import articles
+from flask import Flask, render_template, request
+from src.data import data
 
 # iš flask bibliotekos importuojame klasę Flask ir f-ją render_template.
 app = Flask(__name__)
 # inicijuojame klasės Flask objektą, priskiriame kintamąjam app.
 
-@app.route('/')
+
 # įvelkame f-ją į flask dekoratorių. Be jo  funkcija būtų bereikšmė. Dekorato riaus parametruose nurodome, kad norėsime
 # rezultato 127.0.0.1:8000/ url adrese."""
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', data=articles)
+    if request.method == 'POST':
+        date = request.form['date']
+        autorius = request.form['autorius']
+        tekstas = request.form['tekstas']
+        pavadinimas = request.form['pavadinimas']
+        data.append({
+            'data': date,
+            'autorius': autorius,
+            'pavadinimas': pavadinimas,
+            'tekstas': tekstas,
+            'status': 'published'
+        })
+    return render_template('index.html', data=data)
 # funkcijoje index nurodome, kad norėsime sugeneruoti base.html
 
 
 @app.route('/<string:title>') # parametruose nurodomas kintamasis (title) ir jo tipas (string)
 def article(title): # kintam1jį būtinai nurodykite ir funkcijos parametruose
-    return render_template('article.html', title=title, data=article) # taip pat ir čia reikia jį perduoti
+    return render_template('article.html', title=title, data=data) # taip pat ir čia reikia jį perduoti
+
+
+@app.route('/add_article')
+def add_article():
+    return render_template('add_article.html')
 
 
 if __name__ == '__main__':
